85c12f13f4592221ef74815449ffdeaef2d3eafa
Edy <61327628+Edyzane@users.noreply.github.com>
2022-02-10 14:30:58 +0800

    Add files via upload

---
+++ Data Science basic short ver/sampleModules/my_universal_functions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def checkIfNotNumeric(*args):
+    retVal = True
+    for x in args:
+        if not(isinstance(x,(int,float))):
+            return False
+    return True
+
+def addAllNumerics(*args):
+    s =0
+    for x in args:
+        s+=x
+    return s
+
+myName = "python course"
+
+
+# In[ ]:
+
+
+
+
